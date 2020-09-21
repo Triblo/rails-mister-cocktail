8b2a814fae559fc432eea039805fe24726d465cd
Tricia Blosser <Tricia.blosser@gmail.com>
2020-09-21 13:59:23 -0400

    updated seeds with JSON file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,25 +2,24 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+
+require 'open-uri'
+require 'json'
+
 puts "Cleaning database.."
 Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
 
-puts "Creating ingredients.."
-lemon = { name: "lemon" }
-ice = { name: "ice" }
-mint = { name: "mint leaves" }
-basil = { name: "basil" }
-vodka = { name: "vodka" }
-soda = { name: "soda" }
-olives = { name: "olives" }
-sugar = { name: "simple syrup" }
-rum = { name: "rum" }
-cherry = { name: "grenadine" }
-orange = { name: "Triple Sec" }
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-[ lemon, ice, mint, basil, vodka, soda, olives, sugar, rum, cherry, orange ].each do |attributes|
-  ingredient = Ingredient.create!(attributes)
-  puts "Created #{ingredient.name}"
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredientsList = open(url).read
+ingredients = JSON.parse(ingredientsList)
+
+puts "Saving new ingredients"
+ingredientArray = ingredients["drinks"]
+ingredientArray.each do |ingredient|
+  new_ingredient = Ingredient.new(name: ingredient["strIngredient1"])
+  new_ingredient.save
 end
+
 puts "Finished!"
